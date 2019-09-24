7b5f532fce22edea8246bc708f1c1076e380134e
dem4ply <dem4ply@gmail.com>
2019-09-24 13:34:50 -0500

    update version and add the github url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setup(
     name='chibi_django',
-    version='0.2.0',
+    version='0.2.1',
     description='',
     # long_description=README,
     license='',
@@ -19,8 +19,10 @@ setup(
         'Django>=2.0.7', 'django-filter>=2.0.7',
         'djangorestframework>=3.8.2',
         'django-filters>=0.2.1', 'drf-nested-routers>=0.90.2',
+        'chibi>=0.5.3',
     ],
     dependency_links = [],
+    url='https://github.com/dem4ply/chibi_django',
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
