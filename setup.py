c09407e438134f71f020a67bd71b12bcc83c3d4f
dem4ply <dem4ply@gmail.com>
2026-08-07 16:46:13 -0600

    Bump version: 0.9.0 → 0.10.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name='chibi_django',
-    version='0.9.0',
+    version='0.10.0',
     description='snippets and utilities for django',
     # long_description=README,
     license="WTFPL",
